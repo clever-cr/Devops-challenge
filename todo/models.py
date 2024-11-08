1e947b8ce3bfa0be245911d88f30bcef922e026f
clever-cr <cleverumuhuza@gmail.com>
2024-11-08 22:38:39 +0200

    initialise project

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class ToDo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
